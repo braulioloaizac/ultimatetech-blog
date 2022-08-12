e509bacb47f9eb7e8c9e86a523be6079c01ae133
braulioloaizac <bloaizac@uqvirtual.edu.co>
2022-08-11 23:23:04 -0400

    added homeroute to redirect after signup

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,6 +34,10 @@ router.get('/login', (req, res) => {
 
 router.get('/signup', (req, res) => {
   
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
   res.render('signup');
 
 });
